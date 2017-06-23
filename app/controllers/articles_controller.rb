1efb1e59e511ddbb668c0d3ec1955e8be94b355d
Higman <s15t201@stu.kagawa-u.ac.jp>
2017-06-23 19:22:20 +0900

    add article list

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,20 @@
 class ArticlesController < ApplicationController
   before_action :authenticate_user!, only: [:edit, :delete]
 
-  validates :title, presence: true
-
   def index
     
   end
 
   def show
+  end
+
+  def new
+  end
+
+  def create
+    user = User.find(paramas[:user_id])
+    User.articles.create(params.require(:article).permit(:title, :content))
+    redirect_to user(users)
   end
 
   def edit
--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -2,32 +2,20 @@ require 'rails_helper'
 
 RSpec.describe ArticlesController, type: :controller do
 
-  describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
+  describe "#new" do
+    it "assigns @article" do
+      get :new
+      expect(assigns(:article)).to_not be_nil
     end
   end
 
-  describe "GET #show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
+  describe "#create" do
+    it "create new article" do
+      expect {
+        post :create, article: { title: "test", content: "create_article_test" }
+      }.to change {
+        Article.count
+      }.from(0).to(1)
     end
   end
-
-  describe "GET #edit" do
-    it "returns http success" do
-      get :edit
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET #delete" do
-    it "returns http success" do
-      get :delete
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
